49fbc320beb9675b01e8c75d78399f0f862fc1bf
Nikita Tayade <ntayade9322@gmail.com>
2026-03-11 19:58:07 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub");
         System.out.println("Build triggered using GitHub Webhook");
+        System.out.println("Login to ngrok");
+        
     }
 }
